708268fc54ec2521928aa3a94bec19a8dc35447b
kamrul-huda <kamrul.huda@gmail.com>
2024-02-19 19:47:40 +0600

    modal updated

--- scripts/ticketing.js
+++ scripts/ticketing.js
@@ -1,3 +1,62 @@
+const seats = document.querySelectorAll(".seat");
+const ticketPrice = 550;
+let selectedSeatArr = [];
+let totalTicketPrice = 0;
+let count = 0;
+for (let i = 0; i < seats.length; i++) {
+    const seat = seats[i];
+
+    seat.addEventListener('click', function () {
+        if (selectedSeatArr.length <= 3) {
+            let availableSeat = getElementById('initial-seat');
+            availableSeat -= 1;
+            setElementById('initial-seat', availableSeat);
+
+            const seatNumberElement = document.getElementById(seat.id);
+            const seatNumber = seatNumberElement.innerText;
+            const isExists = selectedSeatArr.includes(seatNumber);
+            if (!isExists) {
+                selectedSeatArr.push(seatNumber);
+
+                totalTicketPrice += ticketPrice;
+
+                setElementById('total-price', totalTicketPrice);
+                setElementById('grand-total', totalTicketPrice);
+
+                const selectedContainer = document.getElementById('selected-seat-container');
+
+                const li = document.createElement("li");
+                const p = document.createElement("p");
+              
+                p.innerText = seatNumber;
+                const p1 = document.createElement("p");
+                p1.innerText = 'Economy';
+                const p2 = document.createElement("p");
+                p2.innerText = ticketPrice;
+
+                li.appendChild(p);
+                li.appendChild(p1);
+                li.appendChild(p2);
+                selectedContainer.appendChild(li);
+                removeBackgroundColorById(seatNumber);
+                addBackgroundColorById(seatNumber);
+
+                count++;
+                setElementById('buy-ticket', count);
+                if (count === 4) {
+                    enableElement('coupon-btn');
+                    enableElement('coupon-text');
+                }
+            }
+            else
+                alert('You already selected this seat.');
+        }
+        else
+            alert('You can not buy more than 4 tickets');
+    })
+}
+
+
 
 function discountCalculate() {
     let discountPrice = 0;
@@ -21,61 +80,4 @@ function discountCalculate() {
         hideElementById('discount-section');
         showElementById('discount');
     }
-}
-
-const seats = document.querySelectorAll(".seat");
-const ticketPrice = 550;
-let selectedSeatArr = [];
-let totalTicketPrice = 0;
-let count = 0;
-for (let i = 0; i < seats.length; i++) {
-    const seat = seats[i];
-
-    seat.addEventListener('click', function () {
-        if (selectedSeatArr.length <= 3) {
-            let availableSeat = getElementById('initial-seat');
-            availableSeat -= 1;
-            setElementById('initial-seat', availableSeat);
-
-            const seatNumber = document.getElementById(seat.id);
-            const isExists = selectedSeatArr.includes(seatNumber.innerText);
-            if (!isExists) {
-                selectedSeatArr.push(seatNumber.innerText);
-
-                totalTicketPrice += ticketPrice;
-
-                setElementById('total-price', totalTicketPrice);
-                setElementById('grand-total', totalTicketPrice);
-
-                const selectedContainer = document.getElementById('selected-seat-container');
-
-                const li = document.createElement("li");
-                const p = document.createElement("p");
-                let sNo = seatNumber.innerText;
-                p.innerText = sNo;
-                const p1 = document.createElement("p");
-                p1.innerText = 'Economy';
-                const p2 = document.createElement("p");
-                p2.innerText = ticketPrice;
-
-                li.appendChild(p);
-                li.appendChild(p1);
-                li.appendChild(p2);
-                selectedContainer.appendChild(li);
-                removeBackgroundColorById(seatNumber.innerText);
-                addBackgroundColorById(seatNumber.innerText);
-
-                count++;
-                setElementById('buy-ticket', count);
-                if (count === 4) {
-                    enableElement('coupon-btn');
-                    enableElement('coupon-text');
-                }
-            }
-            else
-                alert('You already selected this seat.');
-        }
-        else
-            alert('You can not buy more than 4 tickets');
-    })
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -2,14 +2,10 @@ function goToBusTicket() {
     window.location = '#ticket-div';
 }
 
-function getSeats() {
-    const seats = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4', 'I1', 'I2', 'I3', 'I4', 'J1', 'J2', 'J3', 'J4'];
-}
-
 function addBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-[#1DD100]');
-    element.classList.add('text-red');
+    element.classList.add('text-white');
 }
 
 function removeBackgroundColorById(elementId) {
@@ -52,7 +48,7 @@ document.addEventListener('keyup',function(){
 })
 
 function closeModal(){
-    let modal = document.getElementById('my_modal_5');
+    let modal = document.getElementById('my_modal');
     modal.close();
 }
 
